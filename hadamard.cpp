a0880d73a5a485a43d4380261d5aa3466a28bcf1
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-12-13 00:34:54 +0530

    hadadmard matrix code uploaded

---
+++ hadamard.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int p,n=1;
+    cout<<"Enter the power of 2 : ";
+    cin>>p;
+    for(int i=0;i<p;i++)
+    {
+        n*=2;
+    }
+    //int a[n][n];
+    char a[n][n];
+	a[0][0]='*';
+    for(int i=1;i<n;i+=i)
+    {
+        for(int j=0;j<i;j++)
+        {
+            for(int k=0;k<i;k++)
+            {
+                a[j+i][k]=a[j][k];
+                a[j][k+i]=a[j][k];
+                if(a[j][k]=='*')
+                {
+                    a[j+i][k+i]=' ';
+                }
+                else
+                {
+                    a[j+i][k+i]='*';
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
